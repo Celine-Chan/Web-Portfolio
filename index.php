63427d83fa4a50c832a034cf40b315afb2426650
J-BT <jeanbaptiste.theroulde@gmail.com>
2021-08-30 14:22:48 +0200

    + favicon

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
     <!-- Devicon -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
+    <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon"/>
+
     <link rel="stylesheet" href="style.css" />
 
     <title>Jean-Baptiste Theroulde</title>
